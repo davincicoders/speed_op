06d5ff291f250f1f2b2397f6959a4c97957218b2
Jonathan Speek <jonspeek@gmail.com>
2015-12-02 22:11:39 -0700

    Added Validations for URL field. Please don't add development.log and development.sqlite3
    [#109327996]

--- db/migrate/20151130122346_create_identities.rb
+++ db/migrate/20151130122346_create_identities.rb
@@ -1,13 +1,11 @@
 class CreateIdentities < ActiveRecord::Migration
   def change
     create_table :identities do |t|
-      t.string :uid
+      t.belongs_to :user
       t.string :provider
-      t.string :users
 
       t.timestamps null: false
     end
 
-    add_index :identities, :user_id
   end
 end
