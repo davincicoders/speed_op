031a3820555ccadd5aef43444e2fa25c0d6a00b8
Serg09 <sergeyskumatov@aol.com>
2015-12-03 23:20:32 -0700

    Working email form and cron
    [#108834124]

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,9 +3,15 @@ class ContactMailer < ActionMailer::Base
 
   def welcome_email(contact)
     @contact = contact
-    @url = 'http://www.google.com'
+    # @url = 'http://www.google.com'
     attachments['Speed OP.docx'] = File.read('app/assets/files/Speed OP.docx')
-    mail(to: @contact.email, bcc: 'speedopdavinci@gmail.com', subject: 'test welcome email', body: 'no-reply@gmail.com')
+    mail(to: @contact.email, bcc: 'speedopdavinci@gmail.com', subject: 'test welcome email', reply_to: 'no-reply@gmail.com')
 
+    # format.html { render html: 'welcome_email'}
+    # format.text { render text: 'Get a real mail client!'}
+
+    # attachments['Speed OP.docx'] = File.read('/app/assets/files/Speed OP.docx')
+    # attachment :content_type => "images/jpeg",
+    #   :body => File.read("octocat.jpeg")
   end
-  end
+end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,10 +3,13 @@ class UserMailer < ActionMailer::Base
 
   def digest_email_update(user)
     @user = user
-    @url = 'http://www.google.com'
+    @url = 'http://speedop.com'
     attachments['Speed OP.docx'] = File.read('app/assets/files/Speed OP.docx')
-    mail(to: @user.email, bcc: 'speedopdavinci@gmail.com', subject: 'test welcome email', body: 'no-reply@gmail.com')
-
-    end
-
+    attachments.inline['666.txt'] = File.read('app/assets/files/666.txt')
+    attachments.inline['octocat.png'] = File.read('app/assets/images/octocat.png')
+    mail(to: @user.email,
+         bcc: 'speedopdavinci@gmail.com',
+         subject: 'test welcome email')
+  end
 end
+
