ca416c6449789c94fe927907022fea97b7271c82
AnalogRez <sectorz715@gmail.com>
2015-12-04 22:54:49 -0700

    Background Worker pulls from the Google Api Daily
    [#108837862]

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,2 +1,13 @@
 module SitesHelper
+  def up_down(current, previous)
+    current = current.to_i
+    previous = previous.to_i
+    if current > previous
+      "up"
+    elsif current < previous
+      "down"
+    else
+      "even"
+    end
+  end
 end
