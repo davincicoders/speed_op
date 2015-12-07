2e6feae2e06a0b34c176385ab5ce5b03d5107f0e
Riley Jones <rileymjones@gmail.com>
2015-12-06 17:01:17 -0700

    removed unnecessary migrations

--- db/migrate/20151122221506_create_users.rb
+++ db/migrate/20151122221506_create_users.rb
@@ -1,13 +1,20 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :password_digest
-
+      t.string :name
+      t.string :location
+      t.string :image_url
+      t.string :url
+      # provider and uid cannot be null and should be indexed
+      t.string :provider, null: false
+      t.string :uid, null: false
       t.timestamps null: false
     end
+    # adds a clustered index on those two fields
+    # and makes sure that their combination is unique.
+    add_index :users, :provider
+    add_index :users, :uid
+    add_index :users, [:provider, :uid], unique: true
   end
 end
 
--- db/migrate/20151123000112_add_omniauth_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddOmniauthToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :omniauth, :boolean
-  end
-end
