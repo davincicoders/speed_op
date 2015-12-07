e64cbec2babe0fbbc0e4652cb8dd158d3bf69453
Riley Jones <rileymjones@gmail.com>
2015-12-06 21:29:08 -0700

    trying to fix the database problem in heroic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207014917) do
+ActiveRecord::Schema.define(version: 20151207042531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20151207014917) do
   end
 
   add_index "sites", ["user_id"], name: "index_sites_on_user_id", using: :btree
+
+  create_table "structures", force: :cascade do |t|
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
