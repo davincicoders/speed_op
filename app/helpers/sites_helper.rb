133d21edf33e39ee12a90d8661c0810087813669
Lucas Henderson <lucasjhenderson@gmail.com>
2015-12-07 21:11:52 -0700

    removed unused helper methods

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -14,19 +14,5 @@ module SitesHelper
   def stat_call
     @stat_call = @site.webstats.each_with_index
   end
-
-  def mobile_speed
-    @mobile_speed ="[Date.UTC(" + stat.pull_date.getlocal.strftime("%Y,%m,%e") + ")," + stat.mobile_score.to_s + "],"
-  end
-
-  def minus_one
-    (@webstat.pull_date.getlocal.strftime("%m").to_i - 1).to_s
-  end
-
-  def mobile_loop
-    @site.webstats.each do |stat|
-      "[Date.UTC(" + stat.pull_date.getlocal.strftime("%Y, %m, %e") + ")," + stat.mobile_score.to_s + "],".html_safe
-    end
-  end
 end
 
