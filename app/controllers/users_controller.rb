b8d835d078597ccd2e9bad28ecd49f7678a4a552
Riley Jones <rileymjones@gmail.com>
2015-12-09 13:44:23 -0700

    added delayed job gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,16 @@ end
 
   end
 
-  def new
-    @user = User.new
+  def create
+    @user = User.new(user_params)
+    FollowUpEmailJob.new(@user.email).enqueue(wait: 2.seconds)
+
+    if @user.save
+      redirect_to root_path,
+        notice: "Welcome to Speed Op #{@user.first_name.titleize} "
+    else
+      render :new
+    end
   end
 
   def show
