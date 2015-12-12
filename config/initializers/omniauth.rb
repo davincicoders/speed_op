a7df05ff421d55459fbe30fc021c44751a5ecaad
AnalogRez <sectorz715@gmail.com>
2015-12-11 21:58:08 -0700

    Heroku ready

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET']
 
@@ -13,8 +12,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     fields: ['id', 'email-address', 'first-name', 'last-name', 'location',
       'picture-url',
       'public-profile-url']
-
-
+  
 # Google oauth2 was difficult to get working because I had to provide
 # information about the redirect uri
   provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}},
